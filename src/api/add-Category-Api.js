d3bf56da6a5bd157dce0c3028028dbec2e94a7ba
Msahni08 <manojsahni83@gmail.com>
2021-04-08 18:23:27 +0530

    Api created Put,Patch and delete method

--- src/api/add-Category-Api.js
+++ src/api/add-Category-Api.js
@@ -12,7 +12,7 @@ router.get('/',(req,res)=>{
 
 router.get('/getCategory', async (req,res)=>{
     try{
-        var getpass= await passcatgr.find({},{'passcatb':1,'_id':0})
+        var getpass= await passcatgr.find({},{'passcatb':1,'_id':1})
         res.send(getpass)
         console.log(getpass)
     }
@@ -35,5 +35,63 @@ router.post('/sendCategory',async (req,res)=>{
         res.send(err);
  }
 })
+
+router.put('/updateCategory/:_id',async (req,res)=>{
+    try{
+        var updateId=req.params._id;
+        var category=req.body.passcat
+        
+        categoryData= await passcatgr.findById(updateId)
+        categoryData.passcatb=category?category:categoryData.passcatb;
+        categoryData.save();
+        res.send('Update category successfull '+categoryData.passcatb)
+    }
+    catch(err){
+        res.send(err)
+    }
+    
+
+})
+// Note- Difference between Put and patch method is that if we want update only one data even then we have to pass all column data either is will be 1 or 2 or more data
+ // whereas in patch method we pass only those data which to be required for update 
+router.patch('/updateCategory/:_id',async (req,res)=>{
+    try{
+        var updateId=req.params._id;
+        // var updateId=req.body._id;  // if request will be received by body
+        
+        var category=req.body.passcat
+        
+        categoryData= await passcatgr.findById(updateId)
+        categoryData.passcatb=category?category:categoryData.passcatb;
+        categoryData.save();
+        res.send('Update category successfull '+categoryData.passcatb)
+    }
+    catch(err){
+        res.send(err)
+    }
+    
+
+})
+
+router.delete('/updateCategory/:_id',async (req,res)=>{
+    try{
+        var updateId=req.params._id;
+        // var updateId=req.body._id;  // if request will be received by body
+        
+        var category=req.body.passcat
+        
+        categoryData= await passcatgr.findByIdAndRemove(updateId)
+        // categoryData.passcatb=category?category:categoryData.passcatb;
+        categoryData.save();
+        res.send('Deleted category successfull '+categoryData.passcatb)
+    }
+    catch(err){
+        res.send(err)
+    }
+    
+
+})
+
+
 
 module.exports=router
